d4d74d7b7b1010b886132542cb9372663da990ef
nenshibudheliya-coder <nenshibudheliya@gmail.com>
2026-04-06 17:20:59 +0530

    solution this light hose error

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), cssInjectedByJsPlugin()],
+  plugins: [react()],
   server: {
     host: '0.0.0.0',
     port: 5175,
@@ -16,10 +15,13 @@ export default defineConfig({
       output: {
         manualChunks(id) {
           if (id.includes('node_modules')) {
-            return 'vendor';
+            return 'vendor'; // Put libraries in a separate file
           }
-        }
-      }
-    }
-  }
+          if (id.includes('free.jsx') || id.includes('LevelSelect.jsx')) {
+            return 'game-core'; // Put game logic in its own chunk
+          }
+        },
+      },
+    },
+  },
 })
